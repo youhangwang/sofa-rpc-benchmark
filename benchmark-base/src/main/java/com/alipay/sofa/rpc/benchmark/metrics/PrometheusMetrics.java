1449d6a67751c31bd480812a043951d4153ccdfd
youhwsh <youhangwang@foxmail.com>
2026-02-09 10:00:45 +0800

    refine bucket

--- benchmark-base/src/main/java/com/alipay/sofa/rpc/benchmark/metrics/PrometheusMetrics.java
+++ benchmark-base/src/main/java/com/alipay/sofa/rpc/benchmark/metrics/PrometheusMetrics.java
@@ -46,21 +46,20 @@ public class PrometheusMetrics {
         TotalDuration = Histogram.build()
             .name("total_duration_seconds")
             .help("Duration of total in seconds")
-            .buckets(0.01, 0.015, 0.02, 0.025, 0.03, 0.035, 0.04, 0.045, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.2, 0.3)
+            .buckets(0.02, 0.022,0.024,0.026,0.028,0.03,0.032,0.034,0.036,0.038, 0.04, 0.045, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.2, 0.3)
             .register();
 
         computingLogicDuration = Histogram.build()
             .name("user_service_computing_logic_duration_seconds")
             .help("Duration of computing logic execution in seconds")
-            .buckets(0.01, 0.015, 0.02, 0.025, 0.03, 0.035, 0.04, 0.045, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.2, 0.3)
+            .buckets(0.02, 0.022,0.024,0.026,0.028,0.03,0.032,0.034,0.036,0.038, 0.04, 0.045, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.2, 0.3)
             .register();
 
         verifyUserDuration = Histogram
             .build()
             .name("client_verify_user_duration_seconds")
             .help("Duration of verifyUser RPC call including local getUser in seconds")
-            .buckets(0.001, 0.002, 0.003, 0.004, 0.005, 0.006, 0.007, 0.008, 0.009, 0.010, 0.015, 0.025, 0.035, 0.040,
-                0.060, 0.075, 0.100, 0.150, 0.200, 0.500)
+            .buckets(0.0001,0.0002,0.0003,0.0004,0.0005,0.0006,0.0007,0.0008,0.0009,0.001, 0.002, 0.003, 0.004, 0.005)
             .register();
 
         initialized = true;
